f73d5e93a3b6b1dbd8496cb22d5b9696bcfa4717
SecureCoder5 <h5testing0002@gmail.com>
2026-01-05 20:23:26 +0300

    Enhance sendMail function with better SMTP config
    Refactor Mailer.php to improve email sending functionality.

--- app/lib/Mailer.php
+++ app/lib/Mailer.php
@@ -1,27 +1,58 @@
+<?php
+declare(strict_types=1);
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-$mail = new PHPMailer(true);
+/**
+ * Send email using PHPMailer + Gmail SMTP
+ *
+ * @param string $toEmail  Recipient email
+ * @param string $subject  Email subject
+ * @param string $htmlBody HTML body
+ * @return bool
+ */
+function sendMail(string $toEmail, string $subject, string $htmlBody): bool
+{
+    $mail = new PHPMailer(true);
 
-try {
-    $mail->isSMTP();
-    $mail->Host       = $_ENV['MAIL_HOST'];
-    $mail->SMTPAuth   = true;
-    $mail->Username   = $_ENV['MAIL_USERNAME'];
-    $mail->Password   = $_ENV['MAIL_PASSWORD'];
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port       = (int)$_ENV['MAIL_PORT'];
+    try {
+        // SMTP configuration
+        $mail->isSMTP();
+        $mail->Host       = $_ENV['MAIL_HOST'] ?? 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $_ENV['MAIL_USERNAME'];
+        $mail->Password   = $_ENV['MAIL_PASSWORD'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = (int)($_ENV['MAIL_PORT'] ?? 587);
 
-    $mail->setFrom($_ENV['MAIL_FROM'], $_ENV['MAIL_FROM_NAME']);
-    $mail->addAddress($userEmail);
+        // Security & reliability
+        $mail->SMTPDebug  = 0;
+        $mail->CharSet    = 'UTF-8';
+        $mail->Timeout    = 10;
 
-    $mail->isHTML(true);
-    $mail->Subject = 'Verify your email';
-    $mail->Body    = '<p>Your verification code is <b>'.$code.'</b></p>';
+        // Sender
+        $mail->setFrom(
+            $_ENV['MAIL_FROM'],
+            $_ENV['MAIL_FROM_NAME'] ?? 'EasyVault'
+        );
 
-    $mail->send();
-} catch (Exception $e) {
-    error_log('Mail error: ' . $mail->ErrorInfo);
+        // Recipient
+        $mail->addAddress($toEmail);
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $htmlBody;
+        $mail->AltBody = strip_tags($htmlBody);
+
+        $mail->send();
+        return true;
+
+    } catch (Exception $e) {
+        error_log('[MAIL ERROR] ' . $mail->ErrorInfo);
+        return false;
+    }
 }
